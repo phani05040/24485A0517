c44f8bcacd5213cfbad781f18574c1008e9e560c
phani05040 <134404277+phani05040@users.noreply.github.com>
2026-05-09 13:48:17 +0530

    Update Logger.java

--- logging_middleware/Logger.java
+++ logging_middleware/Logger.java
@@ -1,33 +1,12 @@
-class Logger {
-
-    public static void Log(
-            String stack,
-            String level,
-            String packageName,
-            String message) {
-
-        LogRequest request =
-                new LogRequest(
-                        stack,
-                        level,
-                        packageName,
-                        message);
-
-        String response =
-                HttpService.post(
-                        Constants.LOG_API,
-                        request.toJson());
-
-        System.out.println(
-                "\n========== LOG ==========");
-
-        System.out.println(
-                request.toJson());
-
-        System.out.println(
-                response);
-
-        System.out.println(
-                "==========================\n");
+class Logger 
+{
+    public static void Log(String stack,String level,String packageName,String message)
+    {
+        LogRequest request=new LogRequest(stack,level,packageName,message);
+        String response=HttpService.post(Constants.LOG_API,request.toJson());
+        System.out.println("\nThe Hisstory");
+        System.out.println(request.toJson());
+        System.out.println(response);
+        System.out.println("\n This Si absolutely brought from the json query \n");
     }
 }
