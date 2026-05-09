f04fa6a94cce63eb6a9757def40b4ee8d0ec3eba
phani05040 <134404277+phani05040@users.noreply.github.com>
2026-05-09 13:44:46 +0530

    Update LogRequest.java

--- logging_middleware/LogRequest.java
+++ logging_middleware/LogRequest.java
@@ -1,25 +1,18 @@
 class LogRequest
 {
-
     private String stack;
     private String level;
     private String packageName;
     private String message;
-
-    public LogRequest(
-            String stack,
-            String level,
-            String packageName,
-            String message) {
-
+    public LogRequest(String stack,String level,String packageName,String message)
+    {
         this.stack = stack;
         this.level = level;
         this.packageName = packageName;
         this.message = message;
     }
-
-    public String toJson() {
-
+    public String toJson() 
+    {
         return "{"
                 + "\"stack\":\"" + stack + "\","
                 + "\"level\":\"" + level + "\","
